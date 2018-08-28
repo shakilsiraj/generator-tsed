27aeb24c8ab9dbf3661a43da7ab927fc02827b2c
Shakil Siraj <shakil.siraj@gmail.com>
2018-08-28 18:57:36 +1000

    Added swagger definition for controller

--- generators/controller/templates/controller.ts
+++ generators/controller/templates/controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, PathParams } from "@tsed/common";
+import { Description } from "@tsed/swagger";
 
 @Controller("/<%= controllerName %>")
+@Description("Controller endpoints for <%= controllerClassName %>")
 export class <%= controllerClassName %> {
     @Get("/:id")
     async get(@PathParams("id") id: string): Promise<any> {
